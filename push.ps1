0ce600a0271ab1ea5f15af4c53d7b4b5301de8de
Vishnu K R <vishnu.kr.7674@gmail.com>
2026-06-16 12:18:20 +0530

    Configure Matrix CI for Semesters 1-5

--- push.ps1
+++ push.ps1
@@ -1,6 +1,8 @@
-git init
+git add .gitignore
+git rm -r --cached output/
 git add .
-git commit -m "Initialize massive KTU Notes Extractor with Matrix CI"
+git commit -m "Configure Matrix CI for Semesters 1-5"
+git remote remove origin
 git remote add origin https://github.com/vishnuu-kr/ktu_notes.git
 git branch -M main
-git push -u origin main
+git push -u origin main --force
