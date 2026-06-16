17e46f824f962217c8e4eb0b0990a147dd567443
Vishnu K R <vishnu.kr.7674@gmail.com>
2026-06-16 12:19:26 +0530

    Configure Matrix CI for Semesters 1-5

--- push.ps1
+++ push.ps1
@@ -1,8 +1,3 @@
-git add .gitignore
-git rm -r --cached output/
 git add .
 git commit -m "Configure Matrix CI for Semesters 1-5"
-git remote remove origin
-git remote add origin https://github.com/vishnuu-kr/ktu_notes.git
-git branch -M main
 git push -u origin main --force
